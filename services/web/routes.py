b8d6881ac8e1f2b2555e103112b19128fbd38b19
Jasmine <jasmineliu331@gmail.com>
2026-05-05 21:02:22 -0700

    adding web stuff

---
+++ services/web/routes.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi import Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from fastapi import Cookie
+
+# Define the router before using it
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/")
+async def read_root(request: Request):
+    """Returns the HTML content for the home page"""
+    return {"request": request, "username": "hi"}
